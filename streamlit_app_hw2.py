63aa24c0efdbf8711f97c7b4e26a83e35e3cb19e
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-07 09:44:41 +0000

    update hw2

--- streamlit_app_hw2.py
+++ streamlit_app_hw2.py
@@ -9,45 +9,6 @@ import openai
 from openai import AzureOpenAI
 #from dotenv import load_dotenv 
 
-question_to_ask = "Why are LLMs (AI) a danger to society?"
-system_message = """
-Goal: Answer the question using bullets. 
-      The answer should be appropriate for a 10 year old child to understand
-"""
-def output_info(content, start_time, model_info):
-   end_time = time.time()
-   time_taken = end_time - start_time
-   time_taken = round(time_taken * 10)/10
-
-   output = f"For {model_info}, time taken = " + str(time_taken)
-   logging.info(output)
-   logging.info(f"  --> {content}")
-
-   str.write(output)
-
-def do_openAI(model):
-   client = AzureOpenAI(
-      api_version=openai.api_version,
-      azure_endpoint=openai.api_base,
-      api_key=openai_api_key
-   )
-   
-   message_to_LLM = [
-      {"role":"system", "content":system_message},
-      {"role":"user", "content":question_to_ask}
-   ]
-
-   completion = client.chat.completions.create(
-      model = "gpt-4o-mini",
-      messages=message_to_LLM,
-      temperature=0,
-      seed=10,
-      max_tokens=1500,
-      stream=True
-   ) 
-
-   content = ""
-   
 
 def read_url_content(url):
    try:
@@ -85,7 +46,7 @@ else:
 
     question_url = st.text_area(
        "Please insert an URL:",
-       placeholder=""
+       placeholder="",
     )
 
     # Ask the user for a question via `st.text_area`.
@@ -137,3 +98,45 @@ else:
             messages=messages,
             stream=True,
         )
+       
+
+
+question_to_ask = "Why are LLMs (AI) a danger to society?"
+system_message = """
+Goal: Answer the question using bullets. 
+      The answer should be appropriate for a 10 year old child to understand
+"""
+def output_info(content, start_time, model_info):
+   end_time = time.time()
+   time_taken = end_time - start_time
+   time_taken = round(time_taken * 10)/10
+
+   output = f"For {model_info}, time taken = " + str(time_taken)
+   logging.info(output)
+   logging.info(f"  --> {content}")
+
+   str.write(output)
+
+def do_openAI(model):
+   client = AzureOpenAI(
+      api_version=openai.api_version,
+      azure_endpoint=openai.api_base,
+      api_key=openai_api_key
+   )
+   
+   message_to_LLM = [
+      {"role":"system", "content":system_message},
+      {"role":"user", "content":question_to_ask}
+   ]
+
+   completion = client.chat.completions.create(
+      model = "gpt-4o-mini",
+      messages=message_to_LLM,
+      temperature=0,
+      seed=10,
+      max_tokens=1500,
+      stream=True
+   ) 
+
+   content = ""
+   
