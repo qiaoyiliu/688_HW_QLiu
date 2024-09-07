0adf88037e22a0234e0fdf22b49132b6bd33b1ec
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-07 20:09:56 +0000

    update both hw

--- streamlit_app_hw1.py
+++ streamlit_app_hw1.py
@@ -1,18 +1,26 @@
 import streamlit as st
+import openai
+import os
+from openai import OpenAI
 import PyPDF2
 import pdfplumber
-from openai import OpenAI
 from anthropic import Anthropic
 from anthropic.types.message import Message
-import google.generativeai as genai
+from mistralai import Mistral
+import requests
+from bs4 import BeautifulSoup
+#import time
+#import os
+#import logging
+from openai import AzureOpenAI
 
-# Show title and description.
 st.title("Joy's Document question answering for HW1")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+"Upload a document below and ask a question about it – GPT will answer! "
+"To use this app, you need to provide an API key."
 )
 
+#read PDF files
 def read_pdf(pdf_file):
     reader = PyPDF2.PdfReader(pdf_file)
     text = ""
@@ -20,22 +28,45 @@ def read_pdf(pdf_file):
         text += reader.pages[page].extract_text()
     return text
 
+#read URL
+def read_url_content(url):
+     try:
+        response=requests.get(url)
+        response.raise_for_status() 
+        soup=BeautifulSoup(response.content, 'html.parser')
+        return soup.get_text()
+     except requests.RequestException as e:
+        print(f"Error reading {url}:{e}")
+        return None
+
+
+#which LLM selected from sidebar
 def display(selected_llm):
     client = None
 
     if selected_llm == 'gpt-4o-mini':
-        api_key = st.text_input("OpenAI API Key", type="password")
+        #api_key = st.text_input("OpenAI API Key", type="password")
+        api_key = st.secrets['OPENAI_API_KEY']
         if api_key:
             client = OpenAI(api_key=api_key)
         else:
             st.warning("Please provide OpenAI API key")
             return
-    elif selected_llm == 'Claude 3 Opus':
-        api_key = st.text_input("Anthropic API Key", type="password")
+    elif selected_llm == 'claude-3-haiku-20240307':
+        #api_key = st.text_input("Anthropic API Key", type="password")
+        api_key = st.secrets['ANTHROPIC_API_KEY']
         if api_key:
             client = Anthropic(api_key=api_key)
         else:
             st.warning("Please provide Anthropic API key")
+            return
+    elif selected_llm == 'mistral-small-latest':
+        #api_key = st.text_input("Mistral API Key", type="password")
+        api_key = st.secrets['MISTRAL_API_KEY']
+        if api_key:
+            client = Mistral(api_key=api_key)
+        else:
+            st.warning("Please provide Mistral API key")
             return
     #else: 
         #st.info("Please add your OpenAI API key to continue.", icon="🗝️")
@@ -80,59 +111,107 @@ def display(selected_llm):
                     "content": f"Respond in {selected_language}. Here's a document: {document} \n\n---\n\n {question}",
                 }
             ]
+        
+        else:
+            url_content = read_url_content(question_url)
+            messages = [
+                {
+                "role": "user",
+                "content": f"Respond in {selected_language}. Here's a URL: {url_content} \n\n---\n\n {question}",
+                }
+            ]
+        
+        #if using gpt-4o-mini
+        if selected_llm == "gpt-4o-mini":
             stream = client.chat.completions.create(
-            model=selected_llm,
-            messages=messages,
-            stream=True
+                model=selected_llm,
+                max_tokens=250,
+                messages=messages,
+                stream=True,
+                temperature=0.5,
             )
+            
             st.write_stream(stream)
+        
+        elif selected_llm == 'claude-3-haiku-20240307':
+            message = client.messages.create(
+                model=selected_llm,
+                max_tokens=256,
+                messages=messages,
+                temperature=0.5,
+            )
+            data = message.content[0].text
+            st.write(data)
+        
+        elif selected_llm == 'mistral-small-latest':
+            response = client.chat.complete(
+                model=selected_llm,
+                max_tokens=250,
+                messages=messages,
+                temperature=0.5,
+            )
+            data = response.choices[0].message.content
+            st.write(data)
+    
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+
+
+
+
+
+
+
 #openai_api_key = st.text_input("OpenAI API Key", type="password")
-#if not openai_api_key:
-#    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-#else:
+#openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-    # Create an OpenAI client.
-#    client = OpenAI(api_key=openai_api_key)
 
-    # Let the user upload a file via `st.file_uploader`.
-#    uploaded_file = st.file_uploader(
-#        "Upload a document (.txt or .pdf)", type=("txt", "pdf")
-#    )
 
-    # Ask the user for a question via `st.text_area`.
-#    question = st.text_area(
-#        "Now ask a question about the document!",
-#        placeholder="Can you give me a short summary?",
-#        disabled=not uploaded_file,
-#    )
+       
 
-#    if uploaded_file and question:
 
-        # Process the uploaded file and question.
-#        file_extension = uploaded_file.name.split('.')[-1]
-#        if file_extension == 'txt':
-#          document = uploaded_file.read().decode()
-#        elif file_extension == 'pdf':
-#          document = read_pdf(uploaded_file)
-#        else:
-#          st.error("Unsupported file type.")
-#        messages = [
-#            {
-#                "role": "user",
-#                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-#            }
-#        ]
 
-        # Generate an answer using the OpenAI API.
-#        stream = client.chat.completions.create(
-#            model="gpt-4o-mini",
-#            messages=messages,
-#            stream=True,
-#        )
 
-        # Stream the response to the app using `st.write_stream`.
-#        st.write_stream(stream)
+
+
+#question_to_ask = "Why are LLMs (AI) a danger to society?"
+#system_message = """
+#Goal: Answer the question using bullets. 
+#      The answer should be appropriate for a 10 year old child to understand
+#"""
+
+
+
+def output_info(content, start_time, model_info):
+   end_time = time.time()
+   time_taken = end_time - start_time
+   time_taken = round(time_taken * 10)/10
+
+   output = f"For {model_info}, time taken = " + str(time_taken)
+   logging.info(output)
+   logging.info(f"  --> {content}")
+
+   str.write(output)
+
+def do_openAI(model):
+   client = AzureOpenAI(
+      api_version=openai.api_version,
+      azure_endpoint=openai.api_base,
+      api_key=openai_api_key
+   )
+   
+   message_to_LLM = [
+      {"role":"system", "content":system_message},
+      {"role":"user", "content":question_to_ask}
+   ]
+
+   completion = client.chat.completions.create(
+      model = "gpt-4o-mini",
+      messages=message_to_LLM,
+      temperature=0,
+      seed=10,
+      max_tokens=1500,
+      stream=True
+   ) 
+
+   content = ""
+
