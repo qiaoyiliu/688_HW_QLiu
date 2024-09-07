96dac1b4f600b7caec58c4aaa4b1813dd8fd147f
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-07 08:56:12 +0000

    update main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-lab1 = st.Page("streamlit_app_lab1.py", title="Lab 1", icon = ":material/add_circle:")
-lab2 = st.Page("streamlit_app_lab2.py", title="Lab 2", icon=":material/add_circle:")
+hw1 = st.Page("streamlit_app_hw1.py", title="HW 1", icon = ":material/add_circle:")
+hw2 = st.Page("streamlit_app_hw2.py", title="HW 2", icon=":material/add_circle:")
 
-pg = st.navigation([lab1, lab2])
-st.set_page_config(page_title="688Labs", page_icon=":material/edit:")
+pg = st.navigation([hw1, hw2])
+st.set_page_config(page_title="HW Manager", page_icon=":material/edit:")
 pg.run()
