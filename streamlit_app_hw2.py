0832ac1871c75018d79fde0755a0e7d3ca075500
Joy Liu <104224909+qiaoyiliu@users.noreply.github.com>
2024-09-07 09:39:00 +0000

    update hw2

--- streamlit_app_hw2.py
+++ streamlit_app_hw2.py
@@ -1,5 +1,63 @@
 import streamlit as st
 from openai import OpenAI
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+import logging
+import openai
+from openai import AzureOpenAI
+#from dotenv import load_dotenv 
+
+question_to_ask = "Why are LLMs (AI) a danger to society?"
+system_message = """
+Goal: Answer the question using bullets. 
+      The answer should be appropriate for a 10 year old child to understand
+"""
+def output_info(content, start_time, model_info):
+   end_time = time.time()
+   time_taken = end_time - start_time
+   time_taken = round(time_taken * 10)/10
+
+   output = f"For {model_info}, time taken = " + str(time_taken)
+   logging.info(output)
+   logging.info(f"  --> {content}")
+
+   str.write(output)
+
+def do_openAI(model):
+   client = AzureOpenAI(
+      api_version=openai.api_version,
+      azure_endpoint=openai.api_base,
+      api_key=openai_api_key
+   )
+   
+   message_to_LLM = [
+      {"role":"system", "content":system_message},
+      {"role":"user", "content":question_to_ask}
+   ]
+
+   completion = client.chat.completions.create(
+      model = "gpt-4o-mini",
+      messages=message_to_LLM,
+      temperature=0,
+      seed=10,
+      max_tokens=1500,
+      stream=True
+   ) 
+
+   content = ""
+   
+
+def read_url_content(url):
+   try:
+      response=requests.get(url)
+      response.raise_for_status() 
+      soup=BeautifulSoup(response.content, 'html.parser')
+      return soup.get_text()
+   except requests.RequestException as e:
+      print(f"Error reading {url}:{e}")
+      return None
 
 # Show title and description.
 st.title("Joy's Document question answering for HW1")
@@ -22,7 +80,12 @@ else:
 
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .pdf)", type=("txt", "pdf")
+        "Upload a document (.txt, or .pdf)", type=("txt", "pdf")
+    )
+
+    question_url = st.text_area(
+       "Please insert an URL:",
+       placeholder=""
     )
 
     # Ask the user for a question via `st.text_area`.
@@ -56,5 +119,21 @@ else:
             stream=True,
         )
 
+
         # Stream the response to the app using `st.write_stream`.
         st.write_stream(stream)
+
+    elif question_url and question:
+       url_content = read_url_content(question_url)
+       messages = [
+            {
+                "role": "user",
+                "content": f"Here's a URL: {url_content} \n\n---\n\n {question}",
+            }
+        ]
+       
+       stream = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages,
+            stream=True,
+        )
